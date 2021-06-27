ea386a53891b1c5d274c6947a116eb9cc0124547
git-user-111 <user1002233@gmail.com>
2021-06-27 23:34:22 +0700

    Добавил поиск по артикулу

--- src/components/product-search/product-search.component.vue
+++ src/components/product-search/product-search.component.vue
@@ -29,7 +29,10 @@ export default {
       products: [],
       firstProducts: [],
       closedServerLoading: true,
-      serverUrl: 'http://j92342z7.beget.tech/'
+      serverUrl: 'http://j92342z7.beget.tech/',
+      regex1: new RegExp("^\\d{8}$", "g"),
+      regex2: new RegExp("^\\d{3}[,]{1}\\d{3}[,]{1}\\d{2}$", "g"),
+      regex3: new RegExp("^\\d{3}[ ]{1}\\d{3}[ ]{1}\\d{2}$", "g")
     }
   },
   mounted: function () {
@@ -45,76 +48,91 @@ export default {
   {
     getProducts: function() {
       if (this.message !== "") {
+        if (this.message.match(this.regex2)) {
+          this.message = this.message.replaceAll(",", "");
+        }
+        if (this.message.match(this.regex3)) {
+          this.message = this.message.replaceAll(" ", "");
+        }
         this.products = [];
         fetch(`https://www.ikea.com/search/ru/ru/search-result-page?max-num-filters=8&q="${this.message}"&autocorrect=true&size=96&columns=4&sessionId=efd95311-0d91-4801-99e6-7464fd18c5c3&subcategories-style=tree-navigation&columns=%26columns%3D4&types=PRODUCT%2CCONTENT%2CPLANNER%2CREFINED_SEARCHES%2CANSWER&c=sr&v=20210322`)
           .then((response) => {
             return response.json();
-            })
+          })
           .then((response) => {
             this.firstProducts = [];
             response.searchResultPage.products.main.items.forEach(item => {
-              if (item.product && item.type === "PRODUCT") {
-                const name = item.product.name;
-                const priceNumeral = item.product.priceNumeral;
-                if (this.closedServerLoading) {
-                  item.product.gprDescription.variants.forEach(variant => {
-                    this.products.push({
-                      id: variant.id,
-                      name: name,
-                      description: variant.imageAlt,
-                      imageUrl: variant.imageUrl,
-                      link: variant.pipUrl,
-                      priceNumeral: priceNumeral,
-                    })
+            if (item.product && item.type === "PRODUCT") {
+              const name = item.product.name;
+              const priceNumeral = item.product.priceNumeral;
+              if (this.message.match(this.regex1)) {
+                this.products.push({
+                  id: item.product.id,
+                  name: name,
+                  description: item.product.typeName + " " + item.product.itemMeasureReferenceText,
+                  imageUrl: item.product.mainImageUrl,
+                  link: item.product.pipUrl,
+                  priceNumeral: priceNumeral,
+                });
+              } else if (this.closedServerLoading) {
+                item.product.gprDescription.variants.forEach(variant => {
+                  this.products.push({
+                    id: variant.id,
+                    name: name,
+                    description: variant.imageAlt,
+                    imageUrl: variant.imageUrl,
+                    link: variant.pipUrl,
+                    priceNumeral: priceNumeral,
                   });
-                } else {
-                  item.product.gprDescription.variants.forEach(variant => {
-                    this.firstProducts.push({
-                      id: variant.id,
-                      name: name,
-                      description: variant.imageAlt,
-                      imageUrl: variant.imageUrl,
-                      link: variant.pipUrl,
-                      priceNumeral: priceNumeral,
-                    })
-                  });
-                }
+                });
+              } else {
+                item.product.gprDescription.variants.forEach(variant => {
+                  this.firstProducts.push({
+                    id: variant.id,
+                    name: name,
+                    description: variant.imageAlt,
+                    imageUrl: variant.imageUrl,
+                    link: variant.pipUrl,
+                    priceNumeral: priceNumeral,
+                  })
+                });
               }
-            });
-
-            if (!this.closedServerLoading) {
-              this.firstProducts.forEach((firstProduct) => {
-                fetch(`${this.serverUrl}/?url=${firstProduct.link}`, {
-                    mode: 'cors',
-                    method: "POST"
-                  })
-                  .then((response) => {
-                    return response.json();
-                  })
-                  .then((response) => {
-                    console.log(response)
-                    response.products.forEach((product) => {
-                      const cond = this.products.some(function(e){ 
-                        return e.id == product.id;
-                      });
-                      if (!cond) {
-                        this.products.push({
-                          id: product.id,
-                          name: product.title,
-                          description: product.text,
-                          link: product.url,
-                          imageUrl: product.image,
-                          priceNumeral: product.price
-                        })
-                      }
-                    });
-                  })
-                  .catch(err => {
-                    console.log('Failed fetch ' + err);
-                  });
-              });
             }
-          })
+          });
+
+          if (!this.closedServerLoading && this.regex1 == null) {
+            this.firstProducts.forEach((firstProduct) => {
+              fetch(`${this.serverUrl}/?url=${firstProduct.link}`, {
+                  mode: 'cors',
+                  method: "POST"
+                })
+                .then((response) => {
+                  return response.json();
+                })
+                .then((response) => {
+                  console.log(response)
+                  response.products.forEach((product) => {
+                    const cond = this.products.some(function(e){
+                      return e.id == product.id;
+                    });
+                    if (!cond) {
+                      this.products.push({
+                        id: product.id,
+                        name: product.title,
+                        description: product.text,
+                        link: product.url,
+                        imageUrl: product.image,
+                        priceNumeral: product.price
+                      })
+                    }
+                  });
+                })
+                .catch(err => {
+                  console.log('Failed fetch ' + err);
+                });
+            });
+          }
+        });
       }
     }
   }
